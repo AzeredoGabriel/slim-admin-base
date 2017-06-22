4d073953f3a2e01c7e836f2dd52ba42f33f32908
Gabriel Almeida <g.almeida.azeredo@gmail.com>
2017-06-22 00:44:35 -0300

    Adicionando exemplo de responsta JSON

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,5 +10,15 @@ class HomeController extends Controller
 		
 		return $this->view->render($this->response, 'home/index.html.twig', 
 			['name' => $name]); 
+
+		
+		/**
+		 * Para responder no formato json, utilize o exemplo a seguir.
+		 * 
+		 * return $this->response->withJson($data);
+		 * 
+		 */
+		
+		
 	}
 }
